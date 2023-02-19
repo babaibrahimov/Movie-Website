5841c42d73334bdf00d024cdcbb379b6cbaeffcb
babaibrahimov <2295401@student.edu.az>
2023-02-19 17:35:15 +0400

    Movie Website V2.0 (add login box)

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,37 @@ function toggle() {
     video.currentTime = 0;
     video.pause()
 }
+
+let btn = document.getElementById('eye');
+let PasswordInput = document.getElementById('password');
+let body = document.querySelector('body')
+
+let showpassword = true
+btn.addEventListener('click', () => {
+    
+    if (PasswordInput.type === "password") {
+        btn.classList.remove('ri-eye-off-line');
+        btn.classList.add('ri-eye-line');
+        PasswordInput.type = "text";
+    } else {
+        btn.classList.remove('ri-eye-line');
+        btn.classList.add('ri-eye-off-line');
+        PasswordInput.type = "password";
+    }
+
+})
+
+let profileBox = document.querySelector('.profile');
+let profileClose = document.getElementById('profile-close');
+let user = document.getElementById('user');
+
+function showprofile() {
+    profileBox.classList.toggle('active');
+}
+user.addEventListener('click', () => {
+    profileBox.classList.add('active')
+});
+
+profileClose.addEventListener('click', () => {
+    profileBox.classList.remove('active')
+});
